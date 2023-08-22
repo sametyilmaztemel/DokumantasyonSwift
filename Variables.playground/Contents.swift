8a281d3b56014bd26f582e7cef84fbd610dc5746
Samet Yılmaz Temel <115784698+sametyilmaztemel@users.noreply.github.com>
2023-08-22 23:49:21 +0300

    Contents.swift

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -13,3 +13,6 @@ print(ismim)
 var userName = "test"
 var internettenGelenEnYeniVeri = 50 // camelCase
 var internetten_gelen_en_yeni_veri = 40 // snake_case
+
+// Swift diline ait değişkenler kod şablonunu temeli bu şekilde işlemektedir. var ifadesi ile değişken tanımlamaası yapılır
+// değişkenler yazılım dünyasınya oldukça kullanılan verilerdir. 
