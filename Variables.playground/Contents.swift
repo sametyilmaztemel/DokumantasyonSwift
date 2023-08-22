ee111d6d57e055c48b449a6b371924712f787c13
Samet Yılmaz Temel <115784698+sametyilmaztemel@users.noreply.github.com>
2023-08-22 23:35:33 +0300

    Add files via upload

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var ismim = "Samet"
+
+var soyismim = "Temel"
+
+print(ismim)
+
+ismim = "samet"
+
+print(ismim)
+
+var userName = "test"
+var internettenGelenEnYeniVeri = 50 // camelCase
+var internetten_gelen_en_yeni_veri = 40 // snake_case
